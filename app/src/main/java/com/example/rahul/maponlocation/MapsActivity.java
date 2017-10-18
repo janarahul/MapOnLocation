61c6ea424035db620490c863ad88b3ba908be47b
janarahul <janarahul123@outlook.com>
2017-10-18 09:31:11 +0530

    Added Country Toast

--- app/src/main/java/com/example/rahul/maponlocation/MapsActivity.java
+++ app/src/main/java/com/example/rahul/maponlocation/MapsActivity.java
@@ -2,6 +2,8 @@ package com.example.rahul.maponlocation;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -11,6 +13,8 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -20,6 +24,10 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
     LocationManager locationManager;
     LocationListener locationListener;
@@ -28,7 +36,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
             if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,10,10,locationListener);
         }
     }
 
@@ -70,6 +78,20 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 mMap.addMarker(new MarkerOptions().position(ll).title("Marker at your Location").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)));
                 mMap.moveCamera(CameraUpdateFactory.newLatLng(ll));
                 mMap.animateCamera( CameraUpdateFactory.zoomTo( 17.0f ) );
+
+                Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+                try {
+                    List<Address> addressList = geocoder.getFromLocation(location.getLatitude(),location.getLongitude(),1);
+                    if(addressList != null && addressList.size() >0){
+                        Log.i("addressss: ",addressList.get(0).toString());
+                        Address caddress = addressList.get(0);
+
+                        Toast.makeText(MapsActivity.this,"Country:"+caddress.getCountryName()+" pin:"+caddress.getPostalCode() , Toast.LENGTH_SHORT).show();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
 
             @Override
@@ -88,30 +110,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         };
         Location lastLoc = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        if(Build.VERSION.SDK_INT < 23) {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
+        if(lastLoc != null){
             LatLng ll = new LatLng(lastLoc.getLatitude(),lastLoc.getLongitude());
             mMap.clear();
             mMap.addMarker(new MarkerOptions().position(ll).title("Marker at your Location").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)));
             mMap.moveCamera(CameraUpdateFactory.newLatLng(ll));
             mMap.animateCamera( CameraUpdateFactory.zoomTo( 17.0f ) );
+        }
+        if(Build.VERSION.SDK_INT < 23) {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,10,10,locationListener);
+
 
         }else {
             if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
                 //Ask
                 ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.ACCESS_FINE_LOCATION},1);
             }else {
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
-                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0,locationListener);
-                LatLng ll = new LatLng(lastLoc.getLatitude(),lastLoc.getLongitude());
-                mMap.clear();
-                mMap.addMarker(new MarkerOptions().position(ll).title("Marker at your Location").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)));
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(ll));
-                mMap.animateCamera( CameraUpdateFactory.zoomTo( 17.0f ) );
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,10,10,locationListener);
+
+
             }
         }
 
-        // Add a marker in Sydney and move the camera
+
 
     }
 }
